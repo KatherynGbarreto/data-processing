9f776e7648b47756df02d4e458dd179f203e5ffd
Katheryn <Katheryngbarreto@gmil.com>
2023-01-24 21:30:07 -0800

    final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 //https://tinyurl.com/dynamic-html-checker
-
-<script src="js/main.js"></script>   
 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
   
 document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
@@ -17,4 +15,39 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
+
+
+if(key == "Cart"){//cart
+      //alert("Cart Item: " + value);
+ 
+        switch(value){
+          case "Widget":
+            myCart += "<p>Widget: $3.99</p><br>";
+            myTotal += 3.99;
+          break;
+ 
+          case "Sprocket":
+            myCart += "<p>Sprocket: $5.99</p><br>";
+            myTotal += 5.99;
+          break;
+ 
+          case "Thingy":
+            myCart += "<p>Thingy: $1.99</p><br>";
+            myTotal += 1.99;
+          break;
+        }
+ 
+ 
+ 
+    }else{//shipping label
+      //swaps underscores for space
+      key = key.split("_").join(" ");
+      if((key == "FirstName") || (key == "LastName") || (key == "City") || (key == "Address")) {
+        myData += `<p>${key}: ${value.charAt(0).toUpperCase() + value.substring(1).toLowerCase()}</p>`;
+      } else {
+        myData += `<p>${key}: ${value}</p>`;
+      }
+    }
+
 }
+
